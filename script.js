9703fcab777d3f87092c9bf0fa38d3042abe77bb
tjschool <45406017+tjschool@users.noreply.github.com>
2019-09-18 10:13:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function message(message, yes, no) {
 var respond = document.getElementById('respond');
+function message(message, yes, no) {
 if(confirm(message) == true){
 respond.innerHTML = yes;
 }else{
